8c58a3b611557c07dba6a4c68b0bb43d43ad28c6
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2024-01-06 14:10:10 -0500

    splitting on period followed by new line char.

--- src/service/document/parse/clean.js
+++ src/service/document/parse/clean.js
@@ -11,18 +11,26 @@ function removeCitations(content) {
 }
 
 function splitText(text) {
-  let cleanedText = text.replace(/\n/g, " "); // Replace newlines with spaces
-  cleanedText = cleanedText.replace(/\s+/g, " "); // Replace multiple spaces with single space
+  let cleanedText = text;
+
+  // let cleanedText = text.replace(/\n/g, " "); // Replace newlines with spaces
+  // cleanedText = cleanedText.replace(/\s+/g, " "); // Replace multiple spaces with single space
   cleanedText = removeCitations(cleanedText); // Remove citation numbers like [85]
   cleanedText = removeHyperlinks(cleanedText); // Remove hyperlinks
   cleanedText = removeImages(cleanedText); // Remove images
 
-  if (cleanedText.includes(".")) {
+  console.log("Trying to split on new line");
+  if (cleanedText.includes("\x02\n")) {
+    console.log("Inside the if condition");
     // This regex pattern attempts to split the text into sentences based on periods
-    const sentenceRegex = /(?<!\w\.\w)(?<![A-Z][a-z]\.)(?<=[.!?])\s+(?=[A-Z])/g;
-    return cleanedText.split(sentenceRegex);
+    // const sentenceRegex = /(?<!\w\.\w)(?<![A-Z][a-z]\.)(?<=[.!?])\s+(?=[A-Z])/g;
+    let chunks = cleanedText.split("\x02\n");
+
+    console.log("length of chunks is:", chunks.length);
+    // process.exit(0);
+    return chunks;
   }
-  
+
   // there are no periods, so just split by chunks of 100 characters
   const chunkSize = 100;
   const chunks = [];
